a98537567fadd73e1fecd15722229bc4ffe14211
chenang <blogca@qq.com>
2022-03-08 15:34:41 +0800

    新增了MyUtil.h，以及二分查找

---
+++ 数组/MyUtil.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+#include <ctime>
+
+
+namespace MyUtil {
+
+    int* generateRandomArray(int n, int rangeL, int rangeR) {
+
+        assert(n > 0 && rangeL <= rangeR);
+
+        int* arr = new int[n];
+
+        srand(time(NULL));
+        for (int i = 0; i < n; i++)
+            arr[i] = rand() % (rangeR - rangeL + 1) + rangeL;
+        return arr;
+    }
+
+    int* generateOrderedArray(int n) {
+
+        assert(n > 0);
+
+        int* arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = i;
+        return arr;
+    }
+
+}
--- 数组/源.cpp
+++ 数组/源.cpp
@@ -1,6 +1,28 @@
 #include<iostream>
 using namespace std;
-//123
+
+
+//二分查找法的思想在1946年出现，第一个没有bug的二分查找是1962年出现
+
+
+template<typename T>
+int binarySearch(T arr[], int n, T target) {
+
+	int l = 0, r = n - 1; // 在[l,..., r]的范围内寻找target
+	while( l <= r){
+		int mid = (l + r) / 2;
+		if (arr[mid] == target)
+			return mid;
+		if (target > arr[mid])
+			l = mid + 1; //target 在[mid +l, r]中
+		else
+			r = mid - 1;
+
+	} 
+	return -1;
+
+}
+
 int main() {
 
 	system("pause");
